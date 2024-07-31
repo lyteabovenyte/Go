aab4f39b108fb6745754ad36341545f31b7aa2d1
lyteabovenyte <lyteabovenyte@gmail.com>
2024-07-31 18:09:20 +0330

    list and welcome handler

--- partyinvites/main.go
+++ partyinvites/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"html/template"
+	"net/http"
 )
 
 type Rsvp struct {
@@ -16,8 +17,8 @@ var templates = make(map[string]*template.Template, 3)
 func loadTemplates() {
 	templateNames := [5]string{"welcome", "form", "thanks", "sorry", "list"}
 	for index, name := range templateNames {
-		t, err := template.ParseFiles("layout.html", name + ".html")
-		if (err == nil) {
+		t, err := template.ParseFiles("layout.html", name+".html")
+		if err == nil {
 			templates[name] = t
 			fmt.Println("Loaded template:", index, name)
 		} else {
@@ -26,6 +27,17 @@ func loadTemplates() {
 	}
 }
 
+func welcomeHandler(writer http.ResponseWriter, request *http.Request) {
+	templates["welcome"].Execute(writer, nil)
+}
+
+func listHandler(writer http.ResponseWriter, request *http.Request) {
+	templates["list"].Execute(writer, responses)
+}
+
 func main() {
 	loadTemplates()
+
+	http.HandleFunc("/", welcomeHandler)
+	http.HandleFunc("/list", listHandler)
 }
