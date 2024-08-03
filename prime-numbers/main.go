7093966c38396fa11eeea62f479f8fdd8bb11339
lyteabovenyte <lyteabovenyte@gmail.com>
2024-08-04 02:04:39 +0330

    prime numbers between 2 and N

---
+++ prime-numbers/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+// return list of primes less than N
+func sieveOfEratosthenes(N int) (primes []int) {
+	b := make([]bool, N)
+	for i := 2; i < N; i++ {
+		if b[i] == true {
+			continue
+		}
+		primes = append(primes, i)
+		for k := i * i; k < N; k += i {
+			b[k] = true
+		}
+	}
+	return
+}
+
+func main() {
+	primes := sieveOfEratosthenes(100)
+	for _, p := range primes {
+		fmt.Println(p)
+	}
+}
