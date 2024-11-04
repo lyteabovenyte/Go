9cb25ade2f1902dca4558ded9b9339c62e2c8ee4
lyteabovenyte <lyteabovenyte@gmail.com>
2024-11-04 23:33:12 +0330

    printAlloc funciton for benchmarking

--- benchmarking/main.go
+++ benchmarking/main.go
@@ -6,9 +6,11 @@ import (
 )
 
 func main() {
+	printAlloc()
+}
+
+func printAlloc() {
 	var m runtime.MemStats
 	runtime.ReadMemStats(&m)
-	fmt.Println("before GC", m.Alloc/1024, "KB")
-	runtime.GC()
-	fmt.Println("after GC", m.Alloc/1024, "KB")
+	fmt.Println(m.Alloc/1024, "KB")
 }
