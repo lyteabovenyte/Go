e710d9dbc84c8839879109545cdbd0dce7d525a9
lyteabovenyte <lyteabovenyte@gmail.com>
2024-11-05 09:59:26 +0330

    testing...

--- test/go.mod
+++ test/go.mod
@@ -6,4 +6,5 @@ require (
 	golang.org/x/mod v0.21.0 // indirect
 	golang.org/x/sync v0.8.0 // indirect
 	golang.org/x/tools v0.26.0 // indirect
+	github.com/lyteabovenyte/Go_app/benchmarking
 )
--- test/main.go
+++ test/main.go
@@ -1,12 +1,5 @@
 package main
 
-import "fmt"
-
 func main() {
-	var s []string
-	fmt.Println("s --> ", &s)
-	fmt.Println(s == nil)
-	p := []string{}
-	fmt.Println("p -->", &p)
-	fmt.Println(p == nil)
+
 }
