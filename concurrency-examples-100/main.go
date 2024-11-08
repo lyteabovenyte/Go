ed102e3c14035060bb92fac7cc0b888e9b9f156d
lyteabovenyte <lyteabovenyte@gmail.com>
2024-11-08 21:42:58 +0330

    added concurrent mergeChannels function

--- concurrency-examples-100/main.go
+++ concurrency-examples-100/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"unsafe"
 )
 
 func main() {
@@ -9,6 +10,42 @@ func main() {
 	bufferedChannel()
 	fmt.Println("----------unbuffered channel-----------")
 	unBufferedChannel()
+	fmt.Println("--------checking unsafe size of-------")
+	var m struct{}
+	fmt.Println(unsafe.Sizeof(m))
+
+	fmt.Println("------checking whether a channel is open or not-------")
+	// examining whether we are getting an actual message
+	// or a closure event
+	ch1 := make(chan int)
+	close(ch1)
+	v, open := <-ch1
+	fmt.Printf("v: %v, is-open: %v\n", v, open)
+
+	fmt.Println("-----testing merge function--------")
+	channel1 := make(chan int, 2)
+	channel2 := make(chan int, 2)
+
+	finalCh := merge(channel1, channel2)
+	channel1 <- 1
+	channel1 <- 10
+	channel2 <- 2
+	close(channel1)
+	channel2 <- 3
+	channel2 <- 4
+	channel2 <- 5
+	channel2 <- 6
+	channel2 <- 7
+	close(channel2)
+
+	for {
+		value, open := <-finalCh
+		if !open {
+			fmt.Println("channel is closed")
+			break
+		}
+		fmt.Println(value)
+	}
 }
 
 func bufferedChannel() {
@@ -63,4 +100,33 @@ func unBufferedChannel() {
 		uMessageCh <- i
 	}
 	disconnetCh <- struct{}{}
+}
+
+// merge two channels
+// state machine and nil channel approach
+// Receiving from both channels. If one is closed,
+// we assign it to nil so we only receive from one channel
+func merge(ch1, ch2 chan int) (MergeCh chan int) {
+	MergeCh = make(chan int, 5)
+
+	go func() {
+		for ch1 != nil || ch2 != nil {
+			select {
+			case v, open := <-ch1:
+				if !open {
+					ch1 = nil
+					break // it will break the ongoing select not the for loop
+				}
+				MergeCh <- v
+			case v, open := <-ch2:
+				if !open {
+					ch2 = nil
+					break
+				}
+				MergeCh <- v
+			}
+		}
+		close(MergeCh)
+	}()
+	return MergeCh
 }
