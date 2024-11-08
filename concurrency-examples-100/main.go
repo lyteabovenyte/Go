c178811bfaf4cc83df8a26c2a3036f42873658e4
lyteabovenyte <lyteabovenyte@gmail.com>
2024-11-08 19:42:44 +0330

    added unbuffered channel

--- concurrency-examples-100/main.go
+++ concurrency-examples-100/main.go
@@ -5,24 +5,31 @@ import (
 )
 
 func main() {
-	messageCh := make(chan int, 10)
+	fmt.Println("----------buffered channel------------")
+	bufferedChannel()
+	fmt.Println("----------unbuffered channel-----------")
+	unBufferedChannel()
+}
+
+func bufferedChannel() {
+	bmessageCh := make(chan int, 10)
 	disconnectCh := make(chan struct{}, 1)
 
 	for i := 0; i < 10; i++ {
-		messageCh <- i
+		bmessageCh <- i
 	}
 	disconnectCh <- struct{}{}
 
-	// get all the messages from messageCh
+	// get all the messages from bmessageCh
 	// then disconnect respectively
 	for {
 		select {
-		case v := <-messageCh:
+		case v := <-bmessageCh:
 			fmt.Println(v)
 		case <-disconnectCh:
 			for {
 				select {
-				case v := <-messageCh:
+				case v := <-bmessageCh:
 					fmt.Println(v)
 				default:
 					fmt.Println("disconnect signal, return")
@@ -31,4 +38,26 @@ func main() {
 			}
 		}
 	}
+}
+
+func unBufferedChannel() {
+	uMessageCh := make(chan int)
+	disconnetCh := make(chan struct{})
+
+	go func() {
+		for {
+			select {
+			case v := <-uMessageCh:
+				fmt.Println(v)
+			case <-disconnetCh:
+				fmt.Println("disconnet signal, return")
+				return
+			}
+		}
+	}()
+
+	for i := 0; i < 10; i++ {
+		uMessageCh <- i // will block until receiver is ready
+	}
+	disconnetCh <- struct{}{}
 }
