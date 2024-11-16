9ebcc53e21b55bf714c8ca34bf796e2ffc59bd63
lyteabovenyte <lyteabovenyte@gmail.com>
2024-11-17 02:37:10 +0330

    expermineting on benchmarking

--- gopl01/echo/main.go
+++ gopl01/echo/main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
+	"testing"
 )
 
 func main() {
@@ -20,14 +22,88 @@ func main() {
 	fmt.Printf("the type of os.Args --> %T\n", os.Args) // the type of os.Args --> []string
 
 	fmt.Printf("echo2 result -->%s\n", echo2())
+	fmt.Printf("echo with join --> %s\n", echoWithJoin())
+
+	// ex2:
+	for i, ar := range os.Args[1:] {
+		fmt.Println(i, ")", ar)
+	}
+
+	// ex3:
+	testing.Benchmark(func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			slice := []string{"amir", "alaeifar", "ala", "alaeifar", "some", "other", "name"}
+			s := ""
+			for _, arg := range slice {
+				s += arg
+			}
+		}
+		fmt.Println("reporting b.ReportAlloc()")
+		b.ReportAllocs()
+	})
 }
 
 func echo2() string {
 	s := " "
-	sep := 1
-	for _, arg := range os.Args[1:] {
-		s += strconv.Itoa(sep) + ")" + arg + " "
-		sep++
+	for index, arg := range os.Args[1:] {
+		s += strconv.Itoa(index+1) + ")" + arg + " "
 	}
 	return s
 }
+
+func echoWithJoin() string {
+	return strings.Join(os.Args[1:], " ")
+}
+
+/*
+Join function using builder pattern for efficient memory management.
+func Join(elems []string, sep string) string {
+	switch len(elems) {
+	case 0:
+		return ""
+	case 1:
+		return elems[0]
+	}
+
+	var n int
+	if len(sep) > 0 {
+		if len(sep) >= maxInt/(len(elems)-1) {
+			panic("strings: Join output length overflow")
+		}
+		n += len(sep) * (len(elems) - 1)
+	}
+	for _, elem := range elems {
+		if len(elem) > maxInt-n {
+			panic("strings: Join output length overflow")
+		}
+		n += len(elem)
+	}
+
+	var b Builder
+	b.Grow(n)
+	b.WriteString(elems[0])
+	for _, s := range elems[1:] {
+		b.WriteString(sep)
+		b.WriteString(s)
+	}
+	return b.String()
+}
+*/
+
+// var slice = []string{"amir", "alaeifar"}
+
+// // expermineting the time difference between the two.
+// func benchmarkMyEcho2(b *testing.B) {
+// 	for i := 1; i <= b.N; i++ {
+// 		s := ""
+// 		for index, arg := range slice {
+// 			s += strconv.Itoa(index) + arg
+// 		}
+// 	}
+// }
+
+// func benchamrkMyEchoJoin(b *testing.B) {
+// 	for i := 1; i <= b.N; i++ {
+// 		strings.Join(slice, ", ")
+// 	}
+// }
