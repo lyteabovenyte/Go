596f8e2ec891d233a0c8638b49ca0b3ea0832c3f
lyteabovenyte <lyteabovenyte@gmail.com>
2024-11-18 23:45:33 +0330

    making cycle variable global

--- gopl01/server/lissajous.go
+++ gopl01/server/lissajous.go
@@ -11,19 +11,21 @@ import (
 
 var palette = []color.Color{color.White, color.Black}
 
+// var cycles float64 = 5 // number of complete x oscillator revolutions
+
 const (
 	whiteIndex = 0 // first color in palette
 	blackIndex = 1 // next color in palette
 )
 
-func lissajous(out io.Writer) {
+func lissajous(out io.Writer, cycles float64) {
 	const (
-		cycles  = 5     // number of complete x oscillator revolutions
 		res     = 0.001 // angular resolution
 		size    = 100   // image canvas covers [-size..+size]
 		nframes = 64    // number of animation frames
 		delay   = 8     // delay between frames in 10ms units
 	)
+
 	freq := rand.Float64() * 3.0 // relative frequency of y oscillator
 	anim := gif.GIF{LoopCount: nframes}
 	phase := 0.0 // phase difference
