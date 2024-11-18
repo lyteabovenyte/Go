0fe21d1d1c18bfb4693540f201edbd7e32292ff6
lyteabovenyte <lyteabovenyte@gmail.com>
2024-11-18 04:06:56 +0330

    simple server implementation

---
+++ gopl01/server/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe("localhost:8080", nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("a call received.")
+	fmt.Fprintf(w, "Request URL.Path: %q\n", r.URL.Path)
+}
